82585cb3a769002349480009e193ad43a8accf2e
James Hudnall <jimmyhudnall@gmail.com>
2021-02-27 19:32:32 -0500

    Modify ex13 to include user input

--- ex13.py
+++ ex13.py
@@ -1,8 +1,13 @@
 from sys import argv
 
-script, first, second, third = argv
+# script, first, second, third = argv
 
-print('The script is called:', script)
-print('Your first variable is:', first)
-print('Your second variable is:', second)
-print('Your third variable is:', third)
+# print('The script is called:', script)
+# print('Your first variable is:', first)
+# print('Your second variable is:', second)
+# print('Your third variable is:', third)
+
+script, player1, player2, player3 = argv
+rounds = int(input('How many rounds will you play? '))
+
+print(f'So, The three players are {player1}, {player2} and {player3}. You will play {rounds} rounds!')
