79e7a4defe83ffd9d1434d442a2432c4c833d7d6
James Hudnall <jimmyhudnall@gmail.com>
2021-02-27 19:19:37 -0500

    Add incorrect usage of input()

--- ex12.py
+++ ex12.py
@@ -3,3 +3,6 @@ height = input('How tall are you? ')
 weight = input('How much do you weigh? ')
 
 print(f'So, you\'re {age} years old, {height} tall and weigh {weight} lbs.')
+
+# Test
+print('How old are you (again)?', input())
