bd7836233f1f41ea7b9e6de2b86efc85f35d20c8
James Hudnall <jimmyhudnall@gmail.com>
2021-03-01 20:14:32 -0500

    Comment all lines, in accordance with Study Drills #1

--- ex15.py
+++ ex15.py
@@ -1,14 +1,14 @@
-from sys import argv
-script, filename = argv
+from sys import argv # Imports argv function from the sys module
+script, filename = argv # unpacks 2 variables into argv function. First is the script name, second will be used for the filename
 
-txt = open(filename)
+txt = open(filename) # Sets the opening of the file to a variable 'txt'
 
-print(f"Here's your file {filename}:")
-print(txt.read())
+print(f"Here's your file {filename}:") # prints the intro
+print(txt.read()) # Prints the content of the file
 
-print("Type the filename again:")
-file_again=input("> ")
+print("Type the filename again:") # Prints a prompt to the user (without the prompt itself)
+file_again=input("> ") # Displays a prompt to receive input from the user, set to 'file_again' variable
 
-txt_again = open(file_again)
+txt_again = open(file_again) # Sets the opening of the file to a variable
 
-print(txt_again.read())
+print(txt_again.read()) # Prints the content of the second filename specified by the user above
