940b9b9eebf47a6ba45e9b0da92ebf1022bd516c
James Hudnall <jimmyhudnall@gmail.com>
2021-03-07 20:48:00 -0500

    Add exercise 18

---
+++ ex18.py
@@ -0,0 +1,19 @@
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# *args was pointless. We can just do this:
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+# No args
+def print_none():
+    print("I got nothin'")
+
+print_two("James", "Hudnall")
+print_two_again("James", "Hudnall")
+print_one("First!")
+print_none()
