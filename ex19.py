c5a92c50e916eebbe300b82f9fdecd562dac64d2
James Hudnall <jimmyhudnall@gmail.com>
2021-03-10 20:03:00 -0500

    Add comments

--- ex19.py
+++ ex19.py
@@ -1,4 +1,10 @@
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    '''Function that prints out the amount of cheese and crackers.
+
+    Args:
+        cheese_count (int): amount of cheese
+        boxes_of_crackers (int): number of boxes of crackers.
+    '''
     print(f"You have {cheese_count} cheeses!")
     print(f"You have {boxes_of_crackers} boxes of crackers!")
     print("Man, that's enough for a party!")
@@ -15,7 +21,7 @@ amount_of_crackers = 50
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
 print("We can even do math inside too:")
-cheese_and_crackers(10+20, 5+6)
+cheese_and_crackers(10+20, 5+6) 
 
 print("And we can combine the two, variables and math:")
-cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 10)
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 10) # Calls the function 'cheese_and_crackers' with two arguments, plus additional quantities
