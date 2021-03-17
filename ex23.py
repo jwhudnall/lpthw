d2540da54f93743d59539f27cb2666b20a8bfeb8
James Hudnall <jimmyhudnall@gmail.com>
2021-03-16 20:49:09 -0400

    Add exercise 23

---
+++ ex23.py
@@ -0,0 +1,21 @@
+import sys  # Module that interacts strongly with the Python interpreter
+script, encoding, error = sys.argv  # List of command line arguments passed to the script. First = file name
+
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+
+    if line:
+        print_line(line, encoding, errors)
+        return main(language_file, encoding, errors)
+
+def print_line(line, encoding, errors):
+    next_lang = line.strip()
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    cooked_string = raw_bytes.decode(encoding, errors=errors)
+
+    print(raw_bytes, "<===>", cooked_string)
+
+languages = open("languages.txt", encoding="utf-8")
+
+main(languages, encoding, error)
