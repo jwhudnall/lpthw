c4c82a984c5f0c06564e5814640ffd1156f8667c
James Hudnall <jimmyhudnall@gmail.com>
2021-04-03 20:25:14 -0400

    Complete bear room

--- ex35.py
+++ ex35.py
@@ -14,3 +14,25 @@ def gold_room():
         exit(0)
     else: 
         dead("You greedy bastard!")
+
+def bear_room():
+    print("There is a bear here.")
+    print("The bear has a bunch of honey")
+    print("The fat bear is in front of another door.")
+    print("How are you going to move the bear?")
+    bear_moved = False
+    while True:
+        choice = input("> ")
+
+        if choice == "take honey":
+            dead("The bear looks at you then slpas your face.")
+        elif choice == "taunt bear" and not bear_moved:
+            print("The bear has moved from the door.")
+            print("You can go through it now.")
+            bear_moved = True
+        elif choice == "taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews your leg off")
+        elif choice == "open door" and bear_moved:
+            gold_room()
+        else:
+            print("I got no idea what that means.")
