b739cd902482ac3fd9b407f7e8f916dc2964ba54
James Hudnall <jimmyhudnall@gmail.com>
2021-04-03 20:29:59 -0400

    Add start function

--- ex35.py
+++ ex35.py
@@ -54,3 +54,19 @@ def cthulhu_room():
 def dead(why):
     print(why, "Good job!")
     exit(0)
+
+def start():
+    print("You are in a dark room.")
+    print("There is a door to your right and left.")
+    print("Which one do you take?")
+
+    choice = input("> ")
+
+    if choice = "left":
+        bear_room()
+    elif choice == "right":
+        cthulhu_room()
+    else:
+        dead("You stumble around the room until you starve.")
+
+start()
