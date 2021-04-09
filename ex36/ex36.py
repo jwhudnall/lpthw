5e7beba73e9226173bece26bff5eef4215ce753a
James Hudnall <jimmyhudnall@gmail.com>
2021-04-08 20:29:21 -0400

    Remove comments

--- ex36/ex36.py
+++ ex36/ex36.py
@@ -1,13 +1,8 @@
-# Word Choice Game
-
-# Pseudocode
 from sys import exit
 
 
 score = 0
-# Wake up in the morning: 2 choices
-# 1. Exercise
-# 2. Snooze
+
 def wakeup():
     global score
     print("You wake up to your alarm. You have 1 hour before you need to leave for work. Do you get up and exercise, or snooze?")
@@ -24,9 +19,7 @@ def wakeup():
         print("Please choose between \"Exercising\" and \"Snoozing\"")
         wakeup()
 
-# At work, 5 minutes before a big meeting. Coworker asks for help. 2 choices:
-# 1. Help them
-# 2. Tell them you have a meeting to attend.
+
 def meeting_choice():
     global score
     print("You're at work and you have 5 minutes until a big meeting.")
@@ -61,19 +54,7 @@ def end_of_day():
     else:
         print("Invalid choice.")
         end_of_day()
-            
-# *Decision* #
-# Work day ending, and boss asks you to stay late. You know your spouse is at home with your young child and is expecting you.
 
-# 1. Stay at work. lose 1 point. Miss baby's first steps.
-# 2. Go home. Gain 1 point. Boss isn't thrilled, but you convince them you have a spouse and child at home that need your support
-#, so they respect your decision.
-
-# Score Evaluation Function
-# 3 points: On track for a balanced life
-# 1 point: Could improve things.
-# -1 point: Likely not happy
-# -3 points: Should consider reprioritizing things.
 
 def display_score():
     print("*" * 20)
