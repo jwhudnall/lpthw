ca6891e90944e731c1c42a572bcd503df784a1c9
James Hudnall <jimmyhudnall@gmail.com>
2021-04-12 21:07:26 -0400

    Add initial Exercise 39

---
+++ ex39.py
@@ -0,0 +1,22 @@
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California':'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
+
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
+
+# Add more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+# print some cities
+print('-' * 10)
+print('NY State has: ', cities['NY'])
+print('OR State has:', cities['OR'])
